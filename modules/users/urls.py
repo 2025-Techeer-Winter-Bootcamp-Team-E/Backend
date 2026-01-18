6f20bee4d9730eb3de1909c8c93bf2cec21f5b49
Seungheon Cha <tok9663@inu.ac.kr>
2026-01-18 19:53:47 +0900

    feat: 회원가입 + 로그인 api 구현

--- modules/users/urls.py
+++ modules/users/urls.py
@@ -1,14 +1,7 @@
-"""
-Users module URLs.
-"""
 from django.urls import path
-
-from .views import (
-    SignupView,
-    LoginView,
-)
+from .views import UserSignupView, UserLoginview
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='user-signup'),
-    path('login/', LoginView.as_view(), name='user-login'),
+    path('signup/', UserSignupView.as_view(), name='user-signup'),
+    path('login/', UserLoginview.as_view(), name='user-login'),
 ]
