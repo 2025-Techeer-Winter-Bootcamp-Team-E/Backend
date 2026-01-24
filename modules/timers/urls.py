e6d5d5b64edfdfcf7719ff850eac4c313186e3b4
Lyudongwon <a89379903@gmail.com>
2026-01-25 06:15:21 +0900

    타이머 목표가 수정

--- modules/timers/urls.py
+++ modules/timers/urls.py
@@ -2,13 +2,11 @@
 Timers URL configuration.
 """
 from django.urls import path
-
-from .views import TimerListCreateView, TimerDetailView, TimerByProductView
+from .views import TimerListCreateView, TimerDetailView
 
 app_name = 'timers'
 
 urlpatterns = [
     path('', TimerListCreateView.as_view(), name='timer-list-create'),
-    path('detail/<int:timer_id>/', TimerDetailView.as_view(), name='timer-detail'),
-    path('<str:product_code>/', TimerByProductView.as_view(), name='timer-by-product'),
+    path('<int:timer_id>/', TimerDetailView.as_view(), name='timer-detail'),
 ]
